a975f1f539aabfd31aa42789c6a93a8e241b5634
Matt Brogan <broganmatt11@gmail.com>
2020-08-12 10:11:10 -0400

    server update

--- server/index1.js
+++ server/index1.js
@@ -4,7 +4,12 @@ const nodemailer = require('nodemailer')
 
 const app = express()
 
+app.use(express.static(path.join(__dirname, 'build')));
 
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 
 
